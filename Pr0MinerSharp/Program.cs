3f06eddd4bb549b5999dfa3b894c717c77b304d5
Jonas <admin@j-kit.de>
2017-07-24 18:49:54 +0200

    Projektdateien hinzufügen.

---
+++ Pr0MinerSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr0MinerSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
